38bbd5b4dc6d29ead250553aa56a44137b816f30
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-01-08 23:01:21 +0300

    LibrarySpider can parse book names.

--- books_to_scrapy/spiders/library_spider.py
+++ books_to_scrapy/spiders/library_spider.py
@@ -1,6 +1,15 @@
 import scrapy
-
+from scrapy import Selector
 
 
 class LibrarySpider(scrapy.Spider):
-    pass
+    name = 'books_to_scrap'
+    start_urls = ['http://books.toscrape.com/index.html']
+    
+    def parse(self, response, **kwargs):
+        books = response.css('article.product_pod').getall()
+        for book in books:
+            book = Selector(text=book)
+            book_name = book.css('h3')
+            book_name = book_name.css('a::text').get()
+            print(book_name)
