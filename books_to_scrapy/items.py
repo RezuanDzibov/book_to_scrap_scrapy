2642bffcf6d00e75f834b64d186efe9177b1db48
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-01-08 23:29:32 +0300

    Added BookItem.

--- books_to_scrapy/items.py
+++ books_to_scrapy/items.py
@@ -1,12 +1,11 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class BooksToScrapyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class BooksItem(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    image = scrapy.Field()
+    description = scrapy.Field()
+    upc = scrapy.Field()
+    availability = scrapy.Field()
+    tax = scrapy.Field()
